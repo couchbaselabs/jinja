couchbaselabs/jinja

commit 500afbc71dcc9d8ee3b1b1eae68774da39517da3

CBQE-6265: Jinja: Race condition when collecting jenkins data
This condition should only be hit for a few seconds at the end of a job so retry after 10 seconds to get the correct data

Change-Id: If7f83add7d5af7d637daaed3a9c1b5f7670a2ed4
Reviewed-on: http://review.couchbase.org/c/jinja/+/142501
Reviewed-by: Jagadesh Munta <jagadesh.munta@couchbase.com>
Tested-by: Jake Rawsthorne <jake.rawsthorne@couchbase.com>

jinja.py
@@ -291,11 +291,23 @@ def storeTest(jobDoc, view, first_pass=True, lastTotalCount=-1, claimedBuilds=No
                     JOBS[doc["name"]].append(bid)
 
             doc["build_id"] = bid
-            res = getJS(url + str(bid), {"depth": 0})
-            if res is None:
-                continue
 
-            if "result" not in res:
+            should_process = False
+
+            for _ in range(2):
+                res = getJS(url + str(bid), {"depth": 0})
+                if res is None or "result" not in res:
+                    break
+                # retry after 10 seconds if jenkins race condition where result and duration have not been updated to reflect test results
+                # e.g. result set to success, test result processed, result updated, duration updated.
+                if res["duration"] == 0:
+                    print "Sleeping for 10 seconds, potential Jenkins race condition detected..."
+                    time.sleep(10)
+                else:
+                    should_process = True
+                    break
+
+            if not should_process:
                 continue
 
             doc["result"] = res["result"]

commit 9385b822b9e22181ef5b9fc5f51a3fde7673033a

Fix jobs taking long time to be added to bucket.
Issue introduced in 500afbc71dcc9d8ee3b1b1eae68774da39517da3 when trying to fix jenkins race condition. We need to immediately skip jobs without a result otherwise the double 10 second waits happen for every running job

Change-Id: I27097da57fc45013efc1236388f6ca22230c56c0
Reviewed-on: http://review.couchbase.org/c/jinja/+/142742
Tested-by: Bharath G P <bharath.gp@couchbase.com>
Reviewed-by: Bharath G P <bharath.gp@couchbase.com>

jinja.py
@@ -296,7 +296,7 @@ def storeTest(jobDoc, view, first_pass=True, lastTotalCount=-1, claimedBuilds=No
 
             for _ in range(2):
                 res = getJS(url + str(bid), {"depth": 0})
-                if res is None or "result" not in res:
+                if res is None or "result" not in res or res["result"] not in ["SUCCESS", "UNSTABLE", "FAILURE", "ABORTED"]:
                     break
                 # retry after 10 seconds if jenkins race condition where result and duration have not been updated to reflect test results
                 # e.g. result set to success, test result processed, result updated, duration updated.
@@ -312,9 +312,6 @@ def storeTest(jobDoc, view, first_pass=True, lastTotalCount=-1, claimedBuilds=No
 
             doc["result"] = res["result"]
             doc["duration"] = res["duration"]
-
-            if res["result"] not in ["SUCCESS", "UNSTABLE", "FAILURE", "ABORTED"]:
-                continue  # unknown result state
 
             actions = res["actions"]
             params = getAction(actions, "parameters")