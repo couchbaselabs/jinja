b644aeecb59ffeb8e8e4219c95af8bcb6f1715dd
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-01-18 17:55:21 +0000

    Add more log analysis
    Change-Id: I9a67fd6cacef985823e96c7529db1403b83456bb
    Reviewed-on: http://review.couchbase.org/c/jinja/+/143713
    Reviewed-by: Jagadesh Munta <jagadesh.munta@couchbase.com>
    Tested-by: Jake Rawsthorne <jake.rawsthorne@couchbase.com>

--- constants.py
+++ constants.py
@@ -218,9 +218,16 @@ CB_RELEASE_BUILDS = {"0.0.0":"0000",
                      "6.0.2":"2413", "6.0.3":"0000", "6.5.0":"0000"}
 
 CLAIM_MAP = {
-    "git error": ["hudson.plugins.git.GitException"],
+    "git error": ["hudson.plugins.git.GitException", "python3: can't open file 'testrunner.py': [Errno 2] No such file or directory"],
     "SSH error": ["paramiko.ssh_exception.SSHException"],
-    "install failed": ["INSTALL FAILED ON"],
     "IPv6 test on IPv4 host": ["Cannot enable IPv6 on an IPv4 machine"],
-    "Wrong python SDK version": ["ImportError: cannot import name 'N1QLQuery' from 'couchbase.n1ql'"] 
+    "Python SDK error (CBQE-6230)": ["ImportError: cannot import name 'N1QLQuery' from 'couchbase.n1ql'"],
+    "Syntax error": ["KeyError:", "TypeError:"],
+    "json.decoder.JSONDecodeError:": ["json.decoder.JSONDecodeError:"],
+    "ServerUnavailableException: unable to reach the host": ["ServerUnavailableException: unable to reach the host"],
+    "Node already added to cluster": ["ServerAlreadyJoinedException:"],
+    "CBQ Error": ["membase.api.exception.CBQError:", "CBQError: CBQError:"],
+    "RBAC error": ["Cannot assign roles to user because the following roles are unknown, malformed or role parameters are undefined"],
+    "Rebalance error": ["membase.api.exception.RebalanceFailedException"],
+    "install failed": ["INSTALL FAILED ON"],
 }
