f5f99e76ccb093a6574aae227dc334467da54a6a
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-01-21 18:21:54 +0000

    More claims
    Change-Id: Ib9e6beccb8883e1c379ae302d1c947874da47d06
    Reviewed-on: http://review.couchbase.org/c/jinja/+/144084
    Reviewed-by: Jagadesh Munta <jagadesh.munta@couchbase.com>
    Tested-by: Jake Rawsthorne <jake.rawsthorne@couchbase.com>

--- constants.py
+++ constants.py
@@ -219,7 +219,7 @@ CB_RELEASE_BUILDS = {"0.0.0":"0000",
 
 CLAIM_MAP = {
     "git error": ["hudson.plugins.git.GitException", "python3: can't open file 'testrunner.py': [Errno 2] No such file or directory"],
-    "SSH error": ["paramiko.ssh_exception.SSHException"],
+    "SSH error": ["paramiko.ssh_exception.SSHException", "Exception SSH session not active occurred on"],
     "IPv6 test on IPv4 host": ["Cannot enable IPv6 on an IPv4 machine"],
     "Python SDK error (CBQE-6230)": ["ImportError: cannot import name 'N1QLQuery' from 'couchbase.n1ql'"],
     "Syntax error": ["KeyError:", "TypeError:"],
@@ -227,7 +227,10 @@ CLAIM_MAP = {
     "ServerUnavailableException: unable to reach the host": ["ServerUnavailableException: unable to reach the host"],
     "Node already added to cluster": ["ServerAlreadyJoinedException:"],
     "CBQ Error": ["membase.api.exception.CBQError:", "CBQError: CBQError:"],
-    "RBAC error": ["Cannot assign roles to user because the following roles are unknown, malformed or role parameters are undefined"],
+    "RBAC error": ["Exception: {\"errors\":{\"roles\":\"Cannot assign roles to user because the following roles are unknown, malformed or role parameters are undefined: [security_admin]\"}}"],
     "Rebalance error": ["membase.api.exception.RebalanceFailedException"],
+    "Build download failed": ["Unable to copy build to", "Unable to download build in"],
+    "install not started": ["INSTALL NOT STARTED ON"],
     "install failed": ["INSTALL FAILED ON"],
+    "No test report xml": ["ERROR: Step ‘Publish JUnit test result report’ failed: No test report files were found. Configuration error?"]
 }
