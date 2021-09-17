f64384d76adfb00b30f193886b02eab96553d067
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-09-17 18:16:05 +0100

    CBQE-7230: Add Amazon Linux 2 as os to greenboard
    Change-Id: If64376825e03d4df26b2e9c5d0b878d070378a00
    Reviewed-on: http://review.couchbase.org/c/jinja/+/161781
    Tested-by: Bharath G P <bharath.gp@couchbase.com>
    Reviewed-by: Bharath G P <bharath.gp@couchbase.com>

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ UBER_PASS = os.environ.get('UBER_PASS') or ""
 
 ## --- PLATFORMS --- ##
 SERVER_PLATFORMS = ["UBUNTU", "CENTOS", "DEBIAN", "WIN",
-                     "MAC", "SUSE",
-                    "OEL", "DOCKER","RHEL"]
+                    "MAC", "SUSE",
+                    "OEL", "DOCKER", "RHEL", "AMZN2-ARM"]
 SG_PLATFORMS = ["CEN7", "CEN006", "WINDOWS", "MACOSX", "CENTOS"]
 SDK_PLATFORMS = [".NET", "JAVA", "LIBC", "NODE"]
 MOBILE_VERSION = ["1.1.0", "1.2.0", "1.3", "1.4"]
